e144b138ebcff4fe42db3ed6287ed0d3c76ded3a
Roger Stanev <rstanev@gmail.com>
2016-08-29 22:07:18 -0700

    back to zero

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 # Testing GitHub - rstanev@gmail.com
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import requests
+#import pandas as pd
+#import requests
 
 from flask import Flask, render_template, request, redirect
 
