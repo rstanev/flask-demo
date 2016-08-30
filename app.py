79fc62705688254ec23b381d768bb65f181a4e2e
Roger Stanev <rstanev@gmail.com>
2016-08-29 22:44:20 -0700

    back

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
-import requests
-import quandl
+#import requests
+#import quandl
 
 
 from flask import Flask, render_template, request, redirect
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 app.vars={}
 
-quandl.ApiConfig.api_key = 'HWZYrHmanku_oE1Ydh5g'
+#quandl.ApiConfig.api_key = 'HWZYrHmanku_oE1Ydh5g'
 
 @app.route('/')
 def main():
@@ -43,21 +43,21 @@ def index():
 @app.route('/finish')
 def finish_():
 	
-	data = quandl.get('WIKI/%s'%(app.vars['name']))
+	#data = quandl.get('WIKI/%s'%(app.vars['name']))
 
 	#data[app.vars['option']].plot()
 	#plt.show()
 	#plt.savefig('%s.png'%(app.vars['name']))
 		
 	#output_notebook()
-	
+	'''
 	_plot = TimeSeries(data[app.vars['option']], color='blue', legend=True, title='%s Stock Price'%(app.vars['name']))
 	_plot.xaxis.axis_label = 'Date'
 	_plot.yaxis.axis_label = 'Price'
 	
 	output_file('%s.html'%(app.vars['name']))
 	show(_plot)
-	
+	'''
 	#return render_template('end_lulu.html')
 	return redirect('/index')
 	
